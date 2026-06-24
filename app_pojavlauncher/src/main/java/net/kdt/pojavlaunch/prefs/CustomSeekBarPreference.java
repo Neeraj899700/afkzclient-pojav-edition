a07169b57261f625a4b91fc3d84ad9130317a2b4
AFKZ Dev <dev@afkzclient.com>
2026-06-24 14:24:32 +0545

    Fix SeekBarPreference crash: remove custom seekbar code, keep title color only

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/CustomSeekBarPreference.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/CustomSeekBarPreference.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.util.AttributeSet;
-import android.view.View;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,12 +15,8 @@ import git.artdeell.mojo.R;
 
 public class CustomSeekBarPreference extends SeekBarPreference {
 
-    /** The suffix displayed */
-    private String mSuffix = "";
     /** Custom minimum value to provide the same behavior as the usual setMin */
     private int mMin;
-    /** The textview associated by default to the preference */
-    private TextView mTextView;
     /** Seekbar increment in case the max gets set */
     private final int mIncrement;
 
@@ -51,62 +45,15 @@ public class CustomSeekBarPreference extends SeekBarPreference {
 
     @Override
     public void setMin(int min) {
-        //Note: since the max (setMax is a final function) is not taken into account properly, setting the min over the max may produce funky results
         super.setMin(min);
         if (min != mMin) mMin = min;
     }
-
 
     @Override
     public void onBindViewHolder(@NonNull PreferenceViewHolder view) {
         super.onBindViewHolder(view);
         TextView titleTextView = (TextView) view.findViewById(android.R.id.title);
         if(titleTextView != null) titleTextView.setTextColor(Color.WHITE);
-
-        mTextView = (TextView) view.findViewById(R.id.seekbar_value);
-        if(mTextView != null) mTextView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
-        SeekBar seekBar = (SeekBar) view.findViewById(android.R.id.seekbar);
-        if(seekBar == null) seekBar = (SeekBar) view.findViewById(R.id.seekbar);
-        if(seekBar == null) return;
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress += mMin;
-                progress = progress / getSeekBarIncrement();
-                progress = progress * getSeekBarIncrement();
-                progress -= mMin;
-
-                mTextView.setText(String.valueOf(progress + mMin));
-                updateTextViewWithSuffix();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-                int progress = seekBar.getProgress() + mMin;
-                progress /= getSeekBarIncrement();
-                progress *= getSeekBarIncrement();
-                progress -= mMin;
-
-                setValue(progress + mMin);
-                updateTextViewWithSuffix();
-            }
-        });
-
-        updateTextViewWithSuffix();
-    }
-
-    /**
-     * Set a suffix to be appended on the TextView associated to the value
-     * @param suffix The suffix to append as a String
-     */
-    public void setSuffix(String suffix) {
-        this.mSuffix = suffix;
     }
 
     /**
@@ -122,12 +69,5 @@ public class CustomSeekBarPreference extends SeekBarPreference {
     public void setMaxKeepIncrement(int max) {
         super.setMax(max);
         setSeekBarIncrement(mIncrement);
-    }
-
-
-    private void updateTextViewWithSuffix(){
-        if(!mTextView.getText().toString().endsWith(mSuffix)){
-            mTextView.setText(String.format("%s%s", mTextView.getText(), mSuffix));
-        }
     }
 }
